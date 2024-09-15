b8f47cb3dfd3efb0703189dc64f37eb0a397afdd
noura <nourahettaba@gmail.com>
2024-09-16 01:15:24 +0200

    the last update for the php file

--- SenSMS.php
+++ SenSMS.php
@@ -5,17 +5,16 @@
     use Infobip\Model\SmsDestination;
     use Infobip\Model\SmsTextualMessage;
     use Infobip\Model\SmsAdvancedTextualRequest;
-    use Twilio\Rest\Client;
 
     require __DIR__ . "/vendor/autoload.php";
     
     if (!empty($_POST["Nph"]) && !empty($_POST["message"])){
         
-        $destination = $_POST["Nph"] ;
+        $number = $_POST["Nph"] ;
         $message = $_POST["message"] ;
 
-        $base_url = "y3ddmg.api.infobip.com";  //add the URL for your account in the infobiq you can find it in the part of developer 
-        $api_key = "y3ddmg.api.infobip.com"; // add you key 
+        $base_url = "your_url";  //add the URL for your account in the infobiq you can find it in the part of developer 
+        $api_key = "your_key"; // add you key 
         
         $configuration = new Configuration(host: $base_url, apiKey: $api_key);
         $api = new SmsApi(config: $configuration);
@@ -32,6 +31,8 @@
 
         $message="The SMS has been sent successfully.";
         echo "<script type='text/javascript'>alert('$message');</script>";
+        header("Location: indix.html");
+      
     }else {
 
         $message="Please ensure all required information is provided";
